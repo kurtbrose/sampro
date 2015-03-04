68f11834ccb6c6d589acd7d26fa34e48236527be
Kurt Rose <=>
2015-03-03 16:55:47 -0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='sampro',
+      version='0.1',
+      description='sampling profiler',
+      license="MIT",
+      author='Kurt Rose',
+      author_email='kurt@kurtrose.com',
+      url='https://github.com/doublereedkurt/sampro',
+      packages=['sampro'],
+     )
